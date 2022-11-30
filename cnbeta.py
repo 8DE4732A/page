4fb6f3b07d3eaca2bd7c3d3d937684772952832a
pikachu <liuping@zhongan.com>
2022-11-30 12:50:12 +0800

    fix

--- cnbeta.py
+++ cnbeta.py
@@ -1,6 +1,7 @@
 import requests
 import sys
 import hashlib
+from datetime import datetime
 from pathlib import Path
 from bs4 import BeautifulSoup
 
@@ -73,7 +74,7 @@ def parse_artical(url):
         result = url.replace('https://www.cnbeta.com.tw', SERVER_NAME).replace('//hot.cnbeta.com.tw', SERVER_NAME + '/hot')
         
         with open(sys.path[0] + '/artical/' + p, 'w+', encoding='utf-8') as f:
-            f.write('<!DOCTYPE html><html><head><meta name="viewport" content="width=device-width,initial-scale=1"><meta charset="utf-8"><title>' + title + '</title><style>img {max-width: 90%;} body {text-align: center;}</style></head><body>' + str(summary) + '<hr>' + str(content) + '</body></html>')
+            f.write('<!DOCTYPE html><html><head><meta name="viewport" content="width=device-width,initial-scale=1"><meta charset="utf-8"><title>' + title + '</title><style>img {max-width: 90%;} body {text-align: center;}</style></head><body>' + '<h1>' + title + '</h1>' + str(summary) + '<hr>' + str(content) + '</body></html>')
     return SERVER_NAME + '/artical/' + p
 
 
@@ -89,8 +90,9 @@ def parse_index():
             del a['target']
             items.append(str(a))
     articals = '<br/>'.join(items)
+    s = '<p>' + datetime.now().strftime('%Y-%m-%d %H:%M:%S') + '</p>'
     with open(sys.path[0] + '/index.html', 'w+', encoding='utf-8') as f:
-        f.write(f'<!DOCTYPE html><html><head><meta name="viewport" content="width=device-width,initial-scale=1"><meta charset="utf-8"><title>cnbeta</title></head><body>{articals}</body></html>')
+        f.write(f'<!DOCTYPE html><html><head><meta name="viewport" content="width=device-width,initial-scale=1"><meta charset="utf-8"><title>cnbeta</title></head><body>{s}{articals}</body></html>')
         
 
 if __name__ == '__main__':
